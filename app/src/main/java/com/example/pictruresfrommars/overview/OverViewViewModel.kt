f16e83cbc12f02934a6812d26a3503623024190e
FAVOUR <truelyfavoured@gmail.com>
2020-08-18 22:47:27 +0100

    done implementing the value

--- app/src/main/java/com/example/pictruresfrommars/overview/OverViewViewModel.kt
+++ app/src/main/java/com/example/pictruresfrommars/overview/OverViewViewModel.kt
@@ -3,9 +3,14 @@ package com.example.pictruresfrommars.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pictruresfrommars.network.MarsApi
 import com.example.pictruresfrommars.network.MarsApiService
 import com.example.pictruresfrommars.network.MarsProperty
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -18,6 +23,10 @@ class OverViewViewModel : ViewModel() {
     val response: LiveData<String>
         get() = _response
 
+    private val viewModelJob = Job()
+
+    private val coroutineScope = CoroutineScope(viewModelJob + Dispatchers.Main )
+
     /**
      * Call getMarsRealEstateProperties() on init so we can display status immediately.
      */
@@ -29,17 +38,23 @@ class OverViewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsApi.retrofitService.getProperties().enqueue(
-            object :Callback<List<MarsProperty>>{
-                override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                   _response.value="Failure ${t.localizedMessage}"
-                }
 
-                override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
-                    _response.value = "Success: ${response.body()?.size} Mars properties retrieved"
-                }
+       coroutineScope.launch {
+        var listResult =  MarsApi.retrofitService.getProperties()
 
-            }
-        )
+
+           try {
+               _response.value =
+                   "Success: ${listResult.size} Mars properties retrieved"
+           } catch (e: Exception) {
+               _response.value = "Failure: ${e.message}"
+           }
+    }
+    }
+
+
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
     }
 }
