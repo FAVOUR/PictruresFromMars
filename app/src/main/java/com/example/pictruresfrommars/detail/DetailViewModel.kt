7ce27036f514df24347bfd7120f06b0d8015bc9b
FAVOUR <truelyfavoured@gmail.com>
2020-08-18 20:20:38 +0100

    Added MarsProperty class as well as updated the viewmodel

--- app/src/main/java/com/example/pictruresfrommars/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/pictruresfrommars/detail/DetailViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.pictruresfrommars.detail
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.example.pictruresfrommars.network.MarsProperty
 
-class DetailViewModel : ViewModel() {
+/**
+ * The [ViewModel] that is associated with the [DetailFragment].
+ */
+class DetailViewModel(@Suppress("UNUSED_PARAMETER")marsProperty: MarsProperty, app: Application) : AndroidViewModel(app) {
+
     // TODO: Implement the ViewModel
 }
---
+++ app/src/main/java/com/example/pictruresfrommars/network/MarsProperty.kt
@@ -0,0 +1,4 @@
+package com.example.pictruresfrommars.network
+
+class MarsProperty {
+}
