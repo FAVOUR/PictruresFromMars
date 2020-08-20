42886ae0dd460c6918937d0c6a6b4300dbc5e236
FAVOUR <truelyfavoured@gmail.com>
2020-08-20 22:27:24 +0100

    Done Setting up the BindingAdapter.kt

--- app/src/main/java/com/example/pictruresfrommars/BindingAdapter.kt
+++ app/src/main/java/com/example/pictruresfrommars/BindingAdapter.kt
@@ -1,2 +1,25 @@
 package com.example.pictruresfrommars
 
+import android.net.Uri
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestBuilder
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+
+@BindingAdapter("imageUrl")
+    fun ImageView.setImage(imageUrl:String?){
+    imageUrl?.let {
+
+//        var proposedUri = Uri().
+            Glide.with(this.context)
+                .load(imageUrl)
+                .apply(RequestOptions()
+                    .placeholder(R.drawable.loading_animation)
+                    .error(R.drawable.ic_broken_image))
+                .into(this)
+
+
+    }
+       }
